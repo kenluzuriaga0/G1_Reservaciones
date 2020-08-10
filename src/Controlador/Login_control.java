d894d90bdcd4566971ff0797c44b0de9162524b4
Ken Luzuriaga <kenluzuriaga0@hotmail.com>
2020-08-09 19:12:45 -0500

    controlador beginssss

---
+++ src/Controlador/Login_control.java
@@ -0,0 +1,23 @@
+package Controlador;
+
+import reservaciones.Login_view;
+
+
+public class Login_control {
+    
+    
+    private Login_view login;
+    
+    Login_control(Login_view view){
+        this.login = view;
+        
+        
+    }
+    
+    private void initListener(){
+        
+        
+        
+    }
+    
+}
