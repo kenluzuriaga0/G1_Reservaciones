4d82ecb77f9bfef8b8e2a3734a39b6964a3b88c0
Ken Luzuriaga <kenluzuriaga0@hotmail.com>
2020-08-09 20:22:55 -0500

    boton info

--- src/Controlador/Login_control.java
+++ src/Controlador/Login_control.java
@@ -1,8 +1,12 @@
 package Controlador;
 
+import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
 import reservaciones.Login_view;
 import reservaciones.Register_view;
 
@@ -11,7 +15,7 @@ public class Login_control {
     private Login_view login;
     private Register_view register;
 
-    public Login_control(Login_view login ,Register_view register) {
+    public Login_control(Login_view login, Register_view register) {
         this.login = login;
         this.register = register;
         initListener();
@@ -23,7 +27,19 @@ public class Login_control {
         login.getBtn_registrarView().addMouseListener(new Flujo_login());
         login.getBtn_salir().addMouseListener(new Flujo_login());
         register.getBtn_backToLogin().addMouseListener(new Flujo_login());
-        
+        login.getBtn_info().addMouseListener(new Flujo_login());
+        register.getBtn_info().addMouseListener(new Flujo_login());
+
+    }
+
+    private void printInfo() {
+
+        String[] nombres = {"Ken Luzuriaga", "Luis Rodriguez", "Nicole Vera", "Freya Lopez", "Anthony Galarza"};
+        UIManager UI = new UIManager();
+        UI.put("OptionPane.background", new ColorUIResource(29, 53, 87));
+        UI.put("Panel.background", new ColorUIResource(29, 53, 87));
+        UI.put("OptionPane.messageForeground", Color.WHITE);
+        JOptionPane.showMessageDialog(null, nombres, "About", JOptionPane.INFORMATION_MESSAGE);
 
     }
 
@@ -34,19 +50,22 @@ public class Login_control {
 
             Object fuente = e.getSource();
             if (fuente == login.getBtn_salir()) {
-                
+
                 System.exit(0);
-                
+
             } else if (fuente == login.getBtn_registrarView()) {
-                
+
                 login.dispose();
-               
+
                 register.setVisible(true);
-                
-            }else if(fuente == register.getBtn_backToLogin()){
+
+            } else if (fuente == register.getBtn_backToLogin()) {
                 register.dispose();
                 login.setVisible(true);
-                
+
+            } else if (fuente == login.getBtn_info() || fuente == register.getBtn_info()) {
+
+                printInfo();
             }
 
         }
